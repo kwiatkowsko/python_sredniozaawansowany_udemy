154cdc3ebe0ca3619de81957c3f6e7705995a1ba
kwiatkowsko <kwiatkowsko@gmail.com>
2022-05-28 20:30:14 +0200

    28.05

--- automatyczna_konwersja_do_typu_logicznego_3.py
+++ automatyczna_konwersja_do_typu_logicznego_3.py
@@ -1,17 +1,30 @@
-list = ['load data', 'export data', 'analyze & predict']
-a = input("""Choise a number: 
-1 - load data
-2 - export data
-3 - analyze & predict
-Select option above or press enter to exit: """)
-if a == True:
+while True:
+    a = input("""Choise a number:
+                1 - load data
+                2 - export data
+                3 - analyze & predict
+                Select option above or press enter to exit: """)
+
+    if a == "":
+        print("EXIT")
+        break
+    try:
+        a = int(a)
+    except ValueError:
+        print("Must be a int!")
+        continue
+    a = int(a)
+
     if a == 1:
         print("loading data...")
+
     elif a == 2:
         print("exporting data...")
+
     elif a == 3:
-        print("analyze & predict")
+        print("analyze & predict...")
     else:
         print("Must be a number 1-3!")
-else:
-    print("END")
+
+
+
